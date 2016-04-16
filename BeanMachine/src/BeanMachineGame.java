783d61b8bddb012341cd2498624a74c5ded603a9
Sweksha Poudel <Sweksha Poudel>
2016-04-16 02:41:05 -0600

    first push

--- BeanMachine/src/BeanMachineGame.java
+++ BeanMachine/src/BeanMachineGame.java
@@ -12,8 +12,7 @@ public class BeanMachineGame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Gamers!");
 	}
 
 }

176af83cc3d545cfd8aef7b22e6afc8e5e376039
shakyaruchina <shakyar@student.swosu.edu>
2016-04-15 22:40:37 -0500

    first commit

---
+++ workspace/chapter1_hw1/src/Chapter1_hw1.java
@@ -0,0 +1,29 @@
+
+public class Chapter1_hw1 {
+
+	/**This code helps to calculate area
+	 * and perimeter of a rectangle
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Class");
+		System.out.println("\nCalculating the area of"
+				+"a rectangle");
+		System.out.println("width is 4.5");
+		System.out.println("height is 7.9");
+		System.out.println("area is width * height");
+		System.out.println("area is:");
+		System.out.println(4.5 * 7.9);
+		
+		System.out.println("\nCalculating the perimeter of"
+				+"a rectangle");
+		System.out.println("width is 4.5");
+		System.out.println("height is 7.9");
+		System.out.println("area is 2* width + 2 * height");
+		System.out.println("perimeter is:");
+		System.out.println(2*4.5 + 2*7.9);
+
+
+	}
+
+}
---
+++ workspace/chapter1_hw1/src/Series_hw1.java
@@ -0,0 +1,17 @@
+
+public class Series_hw1 {
+
+	/** This code is written to get summation
+	 * of given series
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	   System.out.println("Hello World");
+	   System.out.println("Hello Viewers");
+	   System.out.println("Summation of the given series is:");
+	   System.out.println(1+2+3+4+5+6+7+8+9);
+	  
+
+	}
+
+}
